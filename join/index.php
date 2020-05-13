204e09d738248c61b7bbfa042e6a9d4af283aa33
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-05-13 23:42:04 +0900

    test

---
+++ join/index.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+
+if (!empty($_POST)) {
+	if ($_POST['name'] === '') {
+		$error['name'] = 'blank';
+	}
+	if (strlen($_POST['password']) < 6) {
+		$error['password'] = 'length';
+	}
+	if ($_POST['password'] === '') {
+		$error['password'] = 'blank';
+	}
+	if ($_POST['email'] === '') {
+		$error['email'] = 'blank';
+	}
+	if (strlen($_POST['age']) > 2) {
+		$error['age'] = 'length';
+	}
+	if ($_POST['age'] === '') {
+		$error['age'] = 'blank';
+	}
+	
+	if (empty($error)) {
+			$image = date('YmdHis') . $_FILES['image']['name'];
+			$_SESSION['join'] = $_POST;
+			header('Location: check.php');
+			exit();
+	}
+}
+
+if ($_REQUEST['action'] == 'rewrite'  && isset($_SESSION['join'])) {
+	$_POST = $_SESSION['join'];
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>会員登録画面</title>
+
+	<link rel="stylesheet" href="../style.css" />
+</head>
+
+<body>
+	<div id="wrap">
+		<div id="head">
+			<h1>会員登録画面</h1>
+		</div>
+
+		<div id="content">
+			<p>次のフォームに必要事項をご記入ください。</p>
+			<form action="" method="post" enctype="multipart/form-data">
+				<dl>
+					<dt>ニックネーム<span class="required">必須</span></dt>
+					<dd>
+						<input type="text" name="name" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['name'], ENT_QUOTES)); ?>" />
+						<?php if ($error['name'] === 'blank') : ?>
+							<p class="error">* ニックネームを入力してください</p>
+						<?php endif ?>
+					</dd>
+					<dt>パスワード<span class="required">必須</span></dt>
+					<dd>
+						<input type="password" name="password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
+						<?php if ($error['password'] === 'length') : ?>
+							<p class="error">* パスワードは6文字以上で入力してください</p>
+						<?php endif ?>
+						<?php if ($error['password'] === 'blank') : ?>
+							<p class="error">* パスワードを入力してください</p>
+						<?php endif ?>
+					</dd>
+					<dt>メールアドレス<span class="required">必須</span></dt>
+					<dd>
+						<input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'], ENT_QUOTES)); ?>" />
+						<?php if ($error['email'] === 'blank') : ?>
+							<p class="error">* メールアドレスを入力してください</p>
+						<?php endif ?>
+					<dt>年齢<span class="required">必須</span></dt>
+					<dd>
+						<input type="age" name="age" size="3" maxlength="20" value="<?php print(htmlspecialchars($_POST['age'], ENT_QUOTES)); ?>" />
+					</dd>
+					<?php if ($error['age'] === 'length') : ?>
+							<p class="error">* 適性な年齢を入力してください</p>
+						<?php endif ?>
+					<?php if ($error['age'] === 'blank') : ?>
+							<p class="error">* 年齢を入力してください</p>
+						<?php endif ?>
+					<dt>性別<span class="required">必須</span></dt>
+					<dd>
+						<input type="radio" id="other" name="gender" value="3">
+						<label for="other">指定なし</label><br>
+						<input type="radio" id="male" name="gender" value="0">
+						<label for="male">男性</label><br>
+						<input type="radio" id="female" name="gender" value="1">
+						<label for="female">女性</label><br>
+					</dd>
+					</dd>
+					<dt>結婚<span class="required">必須</span></dt>
+					<dd>
+						<input type="radio" id="married" name="marital_status" value="0">
+						<label for="married">既婚</label><br>
+						<input type="radio" id="unmarried" name="marital_status" value="1">
+						<label for="unmarried">未婚</label><br>
+					</dd>
+
+
+					<dt>アイコン写真など</dt>
+					<dd>
+						<input type="file" name="image" size="35" value="test" />
+					</dd>
+				</dl>
+				<div><input type="submit" value="入力内容を確認する" /></div>
+			</form>
+		</div>
+</body>
+
+</html>
