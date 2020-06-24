af805b25f9682e23259779ec281163f9a2421de4
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-06-24 17:45:43 +0900

    DB修正

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,8 @@
 <?php 
 //データベースに接続
 try {
-  $db = new PDO('mysql:dbname=wordblog_enfight;host=mysql8033.xserver.jp;charset=utf8', 'wordblog_enfight', 'enfight2020');
+  // $db = new PDO('mysql:dbname=wordblog_enfight;host=mysql8033.xserver.jp;charset=utf8', 'wordblog_enfight', 'enfight2020');
+  $db = new PDO('mysql:dbname=Enfight;host=127.0.0.1;port=8889;charset=utf8','root', 'root');
 } catch(PDOException $e) {  //うまく接続できなかった時のエラーメッセージの表示
   print('DB接続エラー:' . $e->getMessage());
 }
