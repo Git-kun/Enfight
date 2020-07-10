0f185fa63ad3cca5d80e15eb7ce766de7fe8418c
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-07-11 02:10:29 +0900

    Update input_do.php

--- input_do.php
+++ input_do.php
@@ -35,16 +35,20 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) { //1時間何�
 }
 
 if (!empty($_POST)) { //もし$_POSTに値が入っていれば
-  if ($_POST['memo'] !== '') { //もしmemoが空でなけらば
-      $statement = $db->prepare('INSERT INTO memos SET memo=?, member_id=?, created_at=NOW()'); //postのデータが安全に渡される為の記述
-      $statement->execute(array($_POST['memo'],$member['id'])); //ここでは上の「?」に入る値を指定する
-      echo 'メッセージが登録されました';
-  } else {
+    if ($_POST['memo'] !== '') { //もしmemoが空でなけらば
+        $statement = $db->prepare('INSERT INTO memos SET memo=?, member_id=?, created_at=NOW()'); //postのデータが安全に渡される為の記述
+        $statement->execute(array($_POST['memo'],$member['id'])); //ここでは上の「?」に入る値を指定する
+        echo 'メッセージが登録されました';
+        // header('Location: input_do.php');
+        // exit();
+    } else {
     echo '*空欄ではメモれません';
-  }
+    }
 }
+
 ?>
 </pre>
+
 <a href="index.php">投稿一覧へ</a>
 |
 <a href="input.php">もう一度メモ</a>
