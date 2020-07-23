7353d226ac37a59c01020449769043c664a86cda
Git-kun <56929408+Git-kun@users.noreply.github.com>
2020-07-23 23:16:11 +0900

    'ログイン画面に戻る'

--- join/index.php
+++ join/index.php
@@ -150,7 +150,7 @@ if ($_REQUEST['action'] == 'rewrite'  && isset($_SESSION['join'])) { //URパラ�
 					</dd>
 				</dl>
 				<div><input type="submit" value="入力内容を確認する" /></div>
-				<a href="../login.php">戻る</a>
+				<a href="../login.php">ログイン画面に戻る</a>
 			</form>
 		</div>
 </body>
